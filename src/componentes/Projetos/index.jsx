7286b9a5e72f72883e495d8b7af9bec4e15b6000
anirutsolrak <carloseduardoturina@gmail.com>
2024-07-25 10:35:06 -0300

    Atualização de projetos

--- src/componentes/Projetos/index.jsx
+++ src/componentes/Projetos/index.jsx
@@ -2,30 +2,42 @@ import React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import ProjetosItens from '../Itens/ProjetosItens';
-import thumb_crud from '../../assets/thumb_crud.jpg'
-import thumb_descript from '../../assets/thumb_descript.jpg'
-import thumb_jogo from '../../assets/thumb_jogo.jpg'
+import thumb_crud from '../../assets/thumb_crud.jpg';
+import thumb_descript from '../../assets/thumb_descript.jpg';
+import thumb_numero from '../../assets/thumb_numero.jpg';
+import thumb_nexus from '../../assets/thumb_nexus.jpg';
 
 const Projetos = () => {
   return (
     <Grid
       container
-      justifyContent="center"
       sx={{ backgroundColor: '#181818', padding: '60px 0', padding: '20px' }}
     >
-      <Grid item xs={12} md={6}>
+      <Grid item xs={12}>
         <Typography
           variant="h2"
           sx={{ textAlign: 'center', marginBottom: '40px', color: '#7CFC00' }}
         >
           Projetos
         </Typography>
-        <Grid container spacing={2} sx={{ gap: '20px' }}>
+      </Grid>
+      <Grid 
+        container
+        sx={{
+          display: 'flex', // Define o display como flex
+          flexDirection: 'row', // Define a direção como linha (row)
+          flexWrap: 'wrap', // Permite que os itens quebrem para a próxima linha
+          gap: '20px', // Define o espaço entre os itens
+          justifyContent: 'space-around', // Distribui os itens uniformemente
+          width: '100%', // Define a largura como 100%
+        }}
+      >
+        <Grid item xs={12} md={5}>
           <ProjetosItens
-            image={thumb_jogo}
+            image={thumb_numero}
             title="Jogo do Número Secreto"
-            description="Um jogo interativo para adivinhar um número secreto, desenvolvido em JavaScript."
-            link="https://jogo-rouge-three.vercel.app"
+            description="Um jogo interativo para adivinhar um número secreto, desenvolvido em React."
+            link="https://numero-secreto-react.vercel.app/"
           />
           <ProjetosItens
             image={thumb_descript}
@@ -33,11 +45,13 @@ const Projetos = () => {
             description="Um aplicativo web que permite criptografar e descriptografar mensagens de texto, garantindo a segurança da informação."
             link="https://encriptacao.vercel.app"
           />
+        </Grid>
+        <Grid item xs={12} md={5}>
           <ProjetosItens
-            image={thumb_jogo}
-            title="Quiz"
-            description="Um quiz interativo que testa seus conhecimentos sobre diversos assuntos, com interface amigável e feedback instantâneo."
-            link="https://quiz-rho-dusky-73.vercel.app"
+            image={thumb_nexus}
+            title="NexusPlay"
+            description="Uma aplicação pra você exibir os seus videos favoritos."
+            link="https://nexuplay-git-main-anirutsolraks-projects.vercel.app/"
           />
           <ProjetosItens
             image={thumb_crud}
@@ -45,7 +59,6 @@ const Projetos = () => {
             description="Um projeto CRUD para gerenciar uma estante virtual de livros, desenvolvido com JavaScript."
             link="https://alura-book-lime-chi.vercel.app/"
           />
-          {/* Adicione mais projetos aqui */}
         </Grid>
       </Grid>
     </Grid>
